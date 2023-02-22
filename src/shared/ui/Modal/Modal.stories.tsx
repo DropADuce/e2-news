edb984cb524d681f5962aa1910e93c1122cfa92e
druce <dropaduce@yandex.ru>
2023-02-23 00:48:01 +0500

    #11 fix loki

--- src/shared/ui/Modal/Modal.stories.tsx
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-
-import { Modal } from './Modal';
+import { Modal } from 'shared/ui/Modal/Modal';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { THEMES } from 'app/providers/ThemeProvider/inedx';
 
@@ -17,14 +16,13 @@ const Template: ComponentStory<typeof Modal> = (args) => <Modal {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-    children: 'storybook example storybook example storybook example storybook example storybook example storybook example storybook example',
     isOpen: true,
+    children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n ',
 };
 
-export const Primary_Dark = Template.bind({});
-Primary_Dark.args = {
-    children: 'storybook example storybook example storybook example storybook example storybook example storybook example storybook example',
+export const Dark = Template.bind({});
+Dark.args = {
     isOpen: true,
+    children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n ',
 };
-Primary_Dark.decorators = [ThemeDecorator(THEMES.DARK)];
-
+Dark.decorators = [ThemeDecorator(THEMES.DARK)];
